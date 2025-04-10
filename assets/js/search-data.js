7dd98eacb91cadcab8887f3b555767dc9a319b5c
KatieLF <katie.l.friese@gmail.com>
2025-04-10 19:16:36 +0000

    Deploying to gh-pages from @ KatieLF/KatieLF.github.io@a68e1c3b762e792537c581a208bf7d43709c4b8a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A curated collection of projects, both productive and puttering",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
